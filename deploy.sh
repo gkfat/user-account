2b594d188e70cbaa69ee956bb186f0d4ed479a56
GK <kevin.wang@atrustek.com>
2022-10-14 17:10:19 +0800

    Update deploy.sh to generate static files and deploy to github page

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
 CURRENT_DATE=$(date +"%Y-%m-%d")
+REMOTE_PATH="https://github.com/gkfat/user-account.git"
+
+#############
+
 echo "Start deploy, $CURRENT_DATE"
 
-echo "Enter commit message: " 
-read COMMIT_MESSAGE
-
-git checkout master
+# 部署至 Remote path gh-page 建立靜態頁面
+git checkout --orphan gh-pages
+npm run build
+cd dist
 git add .
-git commit -m "$COMMIT_MESSAGE"
-git push -f https://github.com/gkfat/user-account.git master
-
-# git checkout --orphan gh-pages
-# npm run build
+git commit -m "Build static files $CURRENT_DATE"
+git push $REMOTE_PATH gh-pages
+cd ..
+rm -rf dist
