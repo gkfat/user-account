d37bd6974ef2d12418c0a1e019db7595e9d4203c
GK <kevin.wang@atrustek.com>
2022-10-14 17:27:52 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
 
-CURRENT_DATE=$(date +"%Y-%m-%d")
-REMOTE_PATH="https://github.com/gkfat/user-account.git"
-
-#############
-
-echo "Start deploy, $CURRENT_DATE"
+CURRENT_DATE=$(date +"%Y-%m-%d %H:%M")
+echo "Start deploy: $CURRENT_DATE"
 
 # 部署至 Remote path gh-page 建立靜態頁面
-git checkout --orphan gh-pages || git checkout gh-pages
 npm run build
-cd dist
-git add .
+git add dist -f
 git commit -m "Build static files $CURRENT_DATE"
-git push $REMOTE_PATH gh-pages
-cd ..
-git checkout master
+git subtree push --prefix dist origin gh-pages
