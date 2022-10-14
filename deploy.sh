e29707ec0f632ab5efb1726d6bbdd8c0df1f3414
GK <kevin.wang@atrustek.com>
2022-10-14 20:30:07 +0800

    Update deploy.sh generate 404.html after build project, avoid github page redirect issue.

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
 CURRENT_DATE=$(date +"%Y-%m-%d %H:%M")
+PROJECT='user-account'
 echo "Start deploy: $CURRENT_DATE"
 
 # 部署至 Remote path gh-page 建立靜態頁面
 npm run build
-git add dist -f
-git commit -m "Build static files $CURRENT_DATE"
+
+# 複製 index.html 並產生一份 404.html
+echo "---
+permalink: /404.html
+---
+" > "./dist/404.html"
+cat "./dist/index.html" >> "./dist/404.html"
+
+# 將 dist 部署至 gh-pages
 git subtree push --prefix dist origin gh-pages
